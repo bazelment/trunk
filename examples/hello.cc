1af326e8282eddcf7542f08d6fea6dd412be8426
Ming Zhao <mzhao@luminatewireless.com>
2015-08-07 15:01:10 -0700

    Hello world with glog and gflags.

---
+++ examples/hello.cc
@@ -0,0 +1,13 @@
+#include "glog/logging.h"
+#include "gflags/gflags.h"
+
+DECLARE_bool(logtostderr);
+int main(int argc, char *argv[]) {
+  FLAGS_logtostderr = true;
+  google::InstallFailureSignalHandler();
+  google::SetUsageMessage("hello");
+  google::ParseCommandLineFlags(&argc, &argv, false);
+  google::InitGoogleLogging(argv[0]);
+  LOG(INFO) << "Hello World!";
+  return 0;
+}
