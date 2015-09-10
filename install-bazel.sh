5f837f999f79de54c0c6f1415eddd209619b5b8c
Ming Zhao <mzhao@luminatewireless.com>
2015-09-10 14:48:40 -0700

    Add script to install released bazel package.

---
+++ install-bazel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+RELEASE=0.1.0
+wget https://github.com/bazelbuild/bazel/releases/download/${RELEASE}/bazel-${RELEASE}-installer-linux-x86_64.sh
+wget https://github.com/bazelbuild/bazel/releases/download/${RELEASE}/bazel-${RELEASE}-installer-linux-x86_64.sh.sha256
+sha256sum -c bazel-${RELEASE}-installer-linux-x86_64.sh.sha256
+chmod +x bazel-${RELEASE}-installer-linux-x86_64.sh
+./bazel-${RELEASE}-installer-linux-x86_64.sh --user
