58957606b1ba60fab01a51e5787b2fc6ce1eecd1
Ming Zhao <mzhao@superdesk.corp.luminatewireless.com>
2017-01-18 16:35:12 -0800

    Use bazel 0.4.2 release.

--- install-bazel.sh
+++ install-bazel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-RELEASE=0.2.2b
+RELEASE=0.4.2
 wget https://github.com/bazelbuild/bazel/releases/download/${RELEASE}/bazel-${RELEASE}-installer-linux-x86_64.sh
 wget https://github.com/bazelbuild/bazel/releases/download/${RELEASE}/bazel-${RELEASE}-installer-linux-x86_64.sh.sha256
 sha256sum -c bazel-${RELEASE}-installer-linux-x86_64.sh.sha256
