2c28f519f39b1a619b59845c5a05a3f89af72980
Ming Zhao <mzhao@luminatewireless.com>
2017-01-18 16:35:03 -0800

    Use the right namespace for gflags.

--- examples/rpc/bank-server.cc
+++ examples/rpc/bank-server.cc
@@ -52,7 +52,7 @@ static void RunServer() {
 
 int main(int argc, char** argv) {
   google::InstallFailureSignalHandler();
-  google::ParseCommandLineFlags(&argc, &argv, false);
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
   google::InitGoogleLogging(argv[0]);
   examples::RunServer();
 
