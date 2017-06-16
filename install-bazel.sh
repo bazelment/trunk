b45dd16ba4a8121e72ab0d66fe4f28aafd2a27f7
Ming Zhao <mzhao@luminatewireless.com>
2017-06-16 14:39:18 -0700

    Switch to bazel 0.5.1

--- install-bazel.sh
+++ install-bazel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-RELEASE=0.4.2
+RELEASE=0.5.1
 wget https://github.com/bazelbuild/bazel/releases/download/${RELEASE}/bazel-${RELEASE}-installer-linux-x86_64.sh
 wget https://github.com/bazelbuild/bazel/releases/download/${RELEASE}/bazel-${RELEASE}-installer-linux-x86_64.sh.sha256
 sha256sum -c bazel-${RELEASE}-installer-linux-x86_64.sh.sha256
