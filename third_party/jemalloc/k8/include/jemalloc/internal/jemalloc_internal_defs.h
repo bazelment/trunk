a791f06ab0f23bb8e1f851b511cd79701dcfd2b1
Ming Zhao <mingzhao@gmail.com>
2019-09-18 21:31:27 -0700

    enable prof in jemalloc with libunwind.

--- third_party/jemalloc/k8/include/jemalloc/internal/jemalloc_internal_defs.h
+++ third_party/jemalloc/k8/include/jemalloc/internal/jemalloc_internal_defs.h
@@ -140,10 +140,10 @@
 /* #undef JEMALLOC_EXPERIMENTAL_SMALLOCX_API */
 
 /* JEMALLOC_PROF enables allocation profiling. */
-/* #undef JEMALLOC_PROF */
+#define JEMALLOC_PROF 
 
 /* Use libunwind for profile backtracing if defined. */
-/* #undef JEMALLOC_PROF_LIBUNWIND */
+#define JEMALLOC_PROF_LIBUNWIND 
 
 /* Use libgcc for profile backtracing if defined. */
 /* #undef JEMALLOC_PROF_LIBGCC */
